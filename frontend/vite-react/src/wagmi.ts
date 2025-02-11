97279985fb996911b993b538b9240d58b6cc883d
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2025-02-11 10:57:33 +0100

    refactor: Added third party RPC to not get errors

--- frontend/vite-react/src/wagmi.ts
+++ frontend/vite-react/src/wagmi.ts
@@ -1,16 +1,16 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { base, mainnet, sepolia } from 'wagmi/chains'
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+
 export const config = createConfig({
   chains: [mainnet, sepolia],
   connectors: [
     injected(),
-    coinbaseWallet(),
-    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
   ],
+  ssr: true,
   transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    [mainnet.id]: http("https://eth-sepolia.g.alchemy.com/v2/TP8LLuBZxjwI3RlpoTNsdImOlO_iLdNo"),
+    [sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/TP8LLuBZxjwI3RlpoTNsdImOlO_iLdNo"),
   },
 })
 
