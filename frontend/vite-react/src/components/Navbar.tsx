73b4ef46f4201c70149bed0fa0d36a7d089b8efd
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2025-02-25 16:42:43 +0100

    feat: Add GameReset event listener to refresh page on game reset

--- frontend/vite-react/src/components/Navbar.tsx
+++ frontend/vite-react/src/components/Navbar.tsx
@@ -2,11 +2,31 @@ import { Button } from "@mantine/core";
 import { useAccount, useDisconnect, useWriteContract } from "wagmi";
 import { abi } from "../utils/abi";
 import { contractAddress } from "../utils/contractAddress";
+import { useEffect, useState } from "react";
+import useWatchContractEventListener from "../hooks/useWatchContractEventListener";
 
 const Navbar = () => {
   const account = useAccount();
   const { disconnect } = useDisconnect();
   const { writeContract } = useWriteContract();
+
+  const [gameReset, setGameReset] = useState<boolean>(false);
+
+  useWatchContractEventListener({
+    eventName: "GameReset",
+    onEvent: (logs: any) => {
+      console.log("GameReset event detected:", logs[0].args);
+      // When the event is detected, set gameReset to true.
+      setGameReset(true);
+    },
+  });
+
+  useEffect(() => {
+    if (gameReset) {
+      // Trigger a page refresh when gameReset becomes true.
+      window.location.reload();
+    }
+  }, [gameReset]);
 
   return (
     <div className="pt-4 pb-12 flex justify-between w-full">
