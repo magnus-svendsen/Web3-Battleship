cbaeded0a9e58a70b1cf856c54e89d2956075764
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2025-02-28 13:57:52 +0100

    feat: Now shows game underway message when two players are already in a game that you are not in

--- frontend/vite-react/src/components/BattleshipGame.tsx
+++ frontend/vite-react/src/components/BattleshipGame.tsx
@@ -5,16 +5,32 @@ import ShipPlacementBoard from "./ShipPlacementBoard";
 import EnemyTerritory from "./EnemyTerritory";
 import { useGameContext } from "../contexts/GameContext";
 import { useEffect, useState } from "react";
-import { useAccount, useReadContract } from "wagmi";
-import { contractAddress } from "../utils/contractAddress";
-import { abi } from "../utils/abi";
+import { useAccount } from "wagmi";
 
 const BattleshipGame = () => {
   const account = useAccount();
 
-  const { setPlayerJoined, moveMessage, turnMessage } = useGameContext();
+  const { firstPlayerJoined, setFirstPlayerJoined, moveMessage, turnMessage } = useGameContext();
 
+  const [secondPlayerJoined, setSecondPlayerJoined] = useState<string>("");
   const [gameStarted, setGameStarted] = useState(false);
+  const [showGameUnderway, setShowGameUnderway] = useState(false);
+
+  const checkGameUnderway = () => {
+    if (
+      firstPlayerJoined !== "0x0000000000000000000000000000000000000000" &&
+      secondPlayerJoined !== "0x0000000000000000000000000000000000000000" &&
+      account.address !== firstPlayerJoined &&
+      account.address !== secondPlayerJoined
+    ) {
+      setShowGameUnderway(true);
+      localStorage.setItem("showGameUnderway", JSON.stringify(true));
+      window.location.reload();
+    } else {
+      setShowGameUnderway(false);
+      localStorage.setItem("showGameUnderway", JSON.stringify(false));
+    }
+  };
 
   useWatchContractEventListener({
     eventName: "GameStarted",
@@ -22,15 +38,25 @@ const BattleshipGame = () => {
       const started = logs[0].args.started ?? false;
       setGameStarted(started);
       localStorage.setItem("gameStarted", JSON.stringify(started));
+      checkGameUnderway();
     },
   });
 
   useWatchContractEventListener({
-    eventName: "PlayerJoined",
+    eventName: "FirstPlayerJoined",
     onEvent: (logs: PlayerJoinedEvent[]) => {
       const player = logs[0].args.player ?? "";
-      setPlayerJoined(player);
-      localStorage.setItem("playerJoined", JSON.stringify(player));
+      setFirstPlayerJoined(player);
+      localStorage.setItem("firstPlayerJoined", JSON.stringify(player));
+    },
+  });
+
+  useWatchContractEventListener({
+    eventName: "SecondPlayerJoined",
+    onEvent: (logs: PlayerJoinedEvent[]) => {
+      const player = logs[0].args.player ?? "";
+      setSecondPlayerJoined(player);
+      localStorage.setItem("secondPlayerJoined", JSON.stringify(player));
     },
   });
 
@@ -39,36 +65,19 @@ const BattleshipGame = () => {
     if (storedGameStarted) {
       setGameStarted(JSON.parse(storedGameStarted));
     }
-    const storedPlayerJoined = localStorage.getItem("playerJoined");
-    if (storedPlayerJoined) {
-      setPlayerJoined(JSON.parse(storedPlayerJoined));
+    const storedFirstPlayerJoined = localStorage.getItem("firstPlayerJoined");
+    if (storedFirstPlayerJoined) {
+      setFirstPlayerJoined(JSON.parse(storedFirstPlayerJoined));
+    }
+    const storedSecondPlayerJoined = localStorage.getItem("secondPlayerJoined");
+    if (storedSecondPlayerJoined) {
+      setSecondPlayerJoined(JSON.parse(storedSecondPlayerJoined));
     }
   }, []);
 
-  const player1 = useReadContract({
-    address: contractAddress,
-    abi,
-    functionName: "player1",
-  });
-
-  const player2 = useReadContract({
-    address: contractAddress,
-    abi,
-    functionName: "player2",
-  });
-
-  const isGameUnderway =
-    player1.data !== "0x0000000000000000000000000000000000000000" &&
-    player2.data !== "0x0000000000000000000000000000000000000000";
-
-  const shouldShowUnderwayMessage =
-    isGameUnderway &&
-    account.address !== player1.data &&
-    account.address !== player2.data;
-
   return (
     <>
-      {shouldShowUnderwayMessage ? (
+      {showGameUnderway ? (
         <h2 className="flex justify-center font-bold text-2xl py-20">
           Game already underway, please wait for the next game.
         </h2>
