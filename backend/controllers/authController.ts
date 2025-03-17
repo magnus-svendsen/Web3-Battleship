117011b6962c8eb9ed89c7d39c5fc67dadbfa08f
magnsven <magnsven@stud.ntnu.no>
2025-03-17 12:57:58 +0100

    fix: removed pub from scope. Added comment about sub

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -15,7 +15,7 @@ export const authWithVipps = async (req: Request, res: Response) => {
   try {
     const nonce = "123hemmelig";
     const state = "321hemmelig";
-    const scope = "phoneNumber pub";
+    const scope = "phoneNumber sub";
 
     const authParams = qs.stringify({
       client_id: VIPPS_CLIENT_ID,
--- backend/models/account.ts
+++ backend/models/account.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const accountSchema = new mongoose.Schema({
+  // The sub is a unique identifier for a Vipps MobilePay user and relates to that user's consent to share information with a specific sales unit.
+  // The sub will not change if a user removes their consents, logs in again, and re-consents.
+  // https://developer.vippsmobilepay.com/docs/APIs/userinfo-api/userinfo-api-guide/
+  
   sub: {
     type: String,
     required: true
