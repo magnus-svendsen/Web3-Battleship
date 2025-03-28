2f55f3892d2cf8abf272435770a41cab1f91eedd
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2025-03-28 12:29:06 +0100

    feat: Use only one reset button that change depending on sinlge or multiplayer mode

--- frontend/vite-react/src/components/Navbar.tsx
+++ frontend/vite-react/src/components/Navbar.tsx
@@ -12,51 +12,58 @@ const Navbar = () => {
   const account = useAccount();
   const { disconnect } = useDisconnect();
 
-  const { autoConfirmTransactions, gameReset, setGameReset, setErrorMessage, setAutoConfirmTransactions, transactionCancelCount } = useGameContext();
+  const {
+    setErrorMessage,
+    autoConfirmTransactions, 
+    setAutoConfirmTransactions, 
+    gameReset, 
+    setGameReset, 
+    transactionCancelCount,
+    mode
+  } = useGameContext();
+
   const executeWriteContract = useGameWriteContract();
-
   const { writeContract } = useWriteContract();
-
   const timeoutRef = useRef<number | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useWatchContractEventListener({
     eventName: "GameReset",
     onEvent: (_logs: GameResetEvent[]) => {
       if (timeoutRef.current) {
         clearTimeout(timeoutRef.current);
-        timeoutRef.current = null
+        timeoutRef.current = null;
       }
       setIsLoading(false);
     },
   });
 
   const handleGameResetMultiplayer = () => {
-    setIsLoading(true)
+    setIsLoading(true);
     timeoutRef.current = window.setTimeout(() => {
-      setIsLoading(false)
+      setIsLoading(false);
       timeoutRef.current = null;
       setErrorMessage("Failed to reset game. Please try again")
     }, 60000); // 60sec timeout if no transaction is validated
     executeWriteContract({ functionName: "resetGame" });
-  }
+  };
 
   const handleGameResetSinglePlayer = () => {
-    setIsLoading(true)
+    setIsLoading(true);
     timeoutRef.current = window.setTimeout(() => {
-      setIsLoading(false)
+      setIsLoading(false);
       timeoutRef.current = null;
       setErrorMessage("Failed to reset game. Please try again")
-    }, 60000); // 60sec timeout if no transaction is validated
-
+    }, 60000);
     writeContract({
       abi: singlePlayerAbi,
       address: singlePlayerContractAddress,
       functionName: "resetGame",
       args: [],
     });
-  }
+  };
 
+  // Listen for reset events (for both modes)
   useWatchContractEvent({
     eventName: "SinglePlayerGameReset",
     address: singlePlayerContractAddress,
@@ -80,23 +87,19 @@ const Navbar = () => {
       localStorage.removeItem("firstPlayerJoined");
       localStorage.removeItem("secondPlayerJoined");
       localStorage.removeItem("showGameUnderway");
-
       localStorage.removeItem("shipPlacementPlayer");
       localStorage.removeItem("grid");
       localStorage.removeItem("shipsSubmitted");
       localStorage.removeItem("placedShips");
-      localStorage.removeItem("bothPlayersPlacedShips");
-
-      localStorage.removeItem("enemyGrid");
       localStorage.removeItem("shipPositions");
+      localStorage.removeItem("bothPlayersPlacedShips");
+      localStorage.removeItem("enemyGrid");
       localStorage.removeItem("moveMessage");
       localStorage.removeItem("turnMessage");
-
       localStorage.removeItem("mode");
       localStorage.removeItem("singlePlayerJoined");
       localStorage.removeItem("singlePlayerShipPlacementPlayer");
-
-      // Then trigger a page refresh.
+      // Then trigger a full page refresh.
       window.location.reload();
     }
   }, [gameReset]);
@@ -105,25 +108,24 @@ const Navbar = () => {
     setIsLoading(false);
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
-      timeoutRef.current = null
+      timeoutRef.current = null;
     }
-  },[transactionCancelCount])
+  }, [transactionCancelCount]);
 
   return (
     <div className="pt-4 pb-12 flex justify-between w-full">
       <h2 className="font-bold text-2xl ml-3">Web3 Battleship</h2>
       {account.status === "connected" && (
-        <div className="flex">
+        <div className="flex items-center">
           {account.connector.id === "privateKey" && (
             <div className="pr-2 pt-2">
-            <Switch
-              checked={autoConfirmTransactions}
-              onChange={(event) => setAutoConfirmTransactions(event.currentTarget.checked)}
-              label="Autoconfirm transactions"
-            />
+              <Switch
+                checked={autoConfirmTransactions}
+                onChange={(event) => setAutoConfirmTransactions(event.currentTarget.checked)}
+                label="Autoconfirm transactions"
+              />
             </div>
           )}
-
           <Button
             variant="white"
             color="teal"
@@ -135,52 +137,39 @@ const Navbar = () => {
           >
             Disconnect
           </Button>
-          {isLoading ?
-            <Button
-              variant="red"
-              color="teal"
-              size="sm"
-              radius="sm"
-              className="mr-2"
-              type="button"
-              disabled={true}
-            > <Loader />
-            </Button>
-            :
-            <Button
-              variant="red"
-              color="teal"
-              size="sm"
-              radius="sm"
-              className="mr-2"
-              type="button"
-              onClick={() => handleGameResetMultiplayer()}
-            >
-              Reset Mulltiplayer
-            </Button>
-          }
-          {isLoading ?
-            <Button
-              color="red"
-              size="sm"
-              radius="sm"
-              className="mr-2"
-              type="button"
-              disabled={true}
-            > <Loader />
-            </Button>
-            :
-            <Button
-              color="red"
-              size="sm"
-              radius="sm"
-              className="mr-2"
-              type="button"
-              onClick={() => handleGameResetSinglePlayer()}
-            >
-              Reset Single Player
-            </Button>
-          }
+          {mode !== "none" && (
+            isLoading ? (
+              <Button
+                variant="red"
+                color="teal"
+                size="sm"
+                radius="sm"
+                className="mr-2"
+                type="button"
+                disabled
+              >
+                <Loader />
+              </Button>
+            ) : (
+              <Button
+                variant="red"
+                color="teal"
+                size="sm"
+                radius="sm"
+                className="mr-2"
+                type="button"
+                onClick={() => {
+                  if (mode === "singleplayer") {
+                    handleGameResetSinglePlayer();
+                  } else if (mode === "multiplayer") {
+                    handleGameResetMultiplayer();
+                  }
+                }}
+              >
+                Reset Game
+              </Button>
+            )
+          )}
         </div>
       )}
     </div>
