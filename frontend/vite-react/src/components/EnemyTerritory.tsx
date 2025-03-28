f45f02173c935a6a323e69d6e7bfcd3d1806d2df
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2025-03-28 02:09:52 +0100

    feat: single player playermove and aimove functions handling

--- frontend/vite-react/src/components/EnemyTerritory.tsx
+++ frontend/vite-react/src/components/EnemyTerritory.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
-import { useAccount } from "wagmi";
+import { useAccount, useWatchContractEvent, useWriteContract } from "wagmi";
 import { useGameContext } from "../contexts/GameContext";
 import { Loader } from "@mantine/core";
 import useGameWriteContract from "../hooks/useGameWriteContract";
 import { useMoveResultListener } from "../hooks/useMoveResultListener";
+import { singlePlayerAbi } from "../utils/abi/singlePlayerAbi";
+import { singlePlayerContractAddress } from "../utils/contractAddress";
+import { intToCoordinate } from "../utils/coordinateUtils";
 
 const EnemyTerritory = () => {
   const account = useAccount();
@@ -11,7 +14,10 @@ const EnemyTerritory = () => {
   useMoveResultListener();
 
   const {
+    grid,
+    setGrid,
     enemyGrid,
+    setEnemyGrid,
     setMoveMessage,
     turnMessage,
     setTurnMessage,
@@ -19,10 +25,12 @@ const EnemyTerritory = () => {
     bothPlayersPlacedShips,
     setErrorMessage,
     moveResultTimeoutRef,
-    transactionCancelCount
+    transactionCancelCount,
+    mode
   } = useGameContext();
 
   const executeWriteContract = useGameWriteContract();
+  const { writeContract } = useWriteContract();
   
   const [loadingCell, setLoadingCell] = useState<{ row: number; col: number } | null>(null);
 
@@ -46,6 +54,75 @@ const EnemyTerritory = () => {
     }
   },[transactionCancelCount])
 
+  useWatchContractEvent({
+    address: singlePlayerContractAddress,
+    abi: singlePlayerAbi,
+    eventName: "MoveResult",
+    onLogs(logs) {
+      const data = logs[0].args;
+        if (typeof data.pos !== "number") {
+          throw new Error("data.pos is undefined");
+        }
+        const coordinate = intToCoordinate(data.pos);
+        let updatedMoveMessage = "";
+        let updatedTurnMessage = "";
+  
+        // Clear loading cell and timer
+        if (moveResultTimeoutRef.current != null) {
+          clearTimeout(moveResultTimeoutRef.current)
+          moveResultTimeoutRef.current = null;
+        }
+  
+        if (data.isPlayerMove) {
+          console.log(data)
+          // Our move: update enemy grid.
+          if (data.hit) {
+            enemyGrid[coordinate.x][coordinate.y] = 3;
+            updatedMoveMessage = data.gameOver ? "You won the game!" : "You shot and hit!";
+          } else {
+            enemyGrid[coordinate.x][coordinate.y] = 2;
+            updatedMoveMessage = "You shot and missed!";
+          }
+          // If the game is over, no next turn.
+          updatedTurnMessage = data.gameOver ? "" : "Opponent's turn";
+          setEnemyGrid(enemyGrid);
+          localStorage.setItem("enemyGrid", JSON.stringify(enemyGrid));
+        } else {
+          console.log(data)
+          // Opponent's move: update our grid.
+          if (data.hit) {
+            grid[coordinate.x][coordinate.y] = 3;
+            updatedMoveMessage = data.gameOver ? "You lost the game!" : "Opponent shot and hit!";
+          } else {
+            grid[coordinate.x][coordinate.y] = 2;
+            updatedMoveMessage = "Opponent shot and missed!";
+          }
+          updatedTurnMessage = data.gameOver ? "" : "Your turn";
+          setGrid(grid);
+          localStorage.setItem("grid", JSON.stringify(grid));
+        }
+  
+        if (moveResultTimeoutRef.current) { clearTimeout(moveResultTimeoutRef.current) }
+        setMoveMessage(updatedMoveMessage);
+        setTurnMessage(updatedTurnMessage);
+  
+        localStorage.setItem("moveMessage", JSON.stringify(updatedMoveMessage));
+        localStorage.setItem("turnMessage", JSON.stringify(updatedTurnMessage));
+
+        // If the game is over, reset the game after 5 seconds
+        if (data.gameOver) {
+          setTimeout(() => {
+            writeContract({
+              abi: singlePlayerAbi,
+              address: singlePlayerContractAddress,
+              functionName: "resetGame",
+              args: [],
+            });
+          }, 5000);
+        }
+    }
+  });
+
   const handleMoveTransaction = (rowIndex: number, colIndex: number) => {
     setLoadingCell({ row: rowIndex, col: colIndex });
 
@@ -57,7 +134,24 @@ const EnemyTerritory = () => {
     }, 60000)
 
     try {
-      executeWriteContract({ functionName: "move", args: [rowIndex, colIndex] });
+      if (mode === "singleplayer") {
+        writeContract({
+          abi: singlePlayerAbi,
+          address: singlePlayerContractAddress,
+          functionName: "playerMove",
+          args: [rowIndex, colIndex],
+        });
+        setTimeout(() => {
+          writeContract({
+            abi: singlePlayerAbi,
+            address: singlePlayerContractAddress,
+            functionName: "aiMove",
+            args: [],
+          });
+        }, 10000);
+      } else {
+        executeWriteContract({ functionName: "move", args: [rowIndex, colIndex] });
+      }
     } catch (error) {
       console.error('Transaction failed:', error);
     }
@@ -86,7 +180,7 @@ const EnemyTerritory = () => {
 
   return (
     <div>
-      {!bothPlayersPlacedShips ? (
+      {!bothPlayersPlacedShips && mode !== "singleplayer" ? (
         <div>
           {shipPlacementPlayer === account.address && (
             <h2>Waiting for opponent to place their ships...</h2>
