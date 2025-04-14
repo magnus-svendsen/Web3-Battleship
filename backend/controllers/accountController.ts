82e93648041a5db2e04af0b15e3bf916fe3127f6
magnsven <magnsven@stud.ntnu.no>
2025-04-14 14:53:06 +0200

    feat: added controller logic for checking if user is verified, and to return the name. Changed the model

--- backend/controllers/accountController.ts
+++ backend/controllers/accountController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import axios from "axios";
-import { generatePrivateKey } from "viem/accounts";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import Account from "../models/account";
 import { VIPPS_USERINFO_URL } from "../config/env";
 
@@ -15,13 +15,17 @@ export const getOrGeneratePrivateKey = async (req: Request, res: Response) => {
       },
     });
     const sub = userInfoResponse.data.sub;
+    const name = userInfoResponse.data.name; // TODO: Store name in DB, so that it can be returned for opponent to see. 
+
     let account = await Account.findOne({ sub });
 
     if (account) {
       res.status(200).json(account.privateKey);
     } else {
       const newPrivateKey = generatePrivateKey();
-      account = await Account.create({ sub, privateKey: newPrivateKey });
+      const address = privateKeyToAccount(newPrivateKey).address;
+
+      account = await Account.create({ sub, privateKey: newPrivateKey, address: address, name: name });
       res.json(newPrivateKey);
     }
   } catch (error) {
@@ -43,6 +47,28 @@ export const getUserInfo = async (req: Request, res: Response) => {
     res.json(userInfoResponse.data)
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Failed to retrieve user info"})
+    res.status(500).json({ error: "Failed to retrieve user info" })
+  }
+}
+
+
+export const checkIfAddressIsVerified = async (req: Request, res: Response) => {
+  try {
+    const address_to_check = req.body["address"];
+    let account = await Account.findOne({ address_to_check });
+    if (account) {
+      res.status(200).json({
+        verified: true,
+        name: account.name
+      })
+    }
+    else {
+      res.status(404).json({
+        verified: false,
+      })
+    }
+  }
+  catch {
+    res.status(500)
   }
 }
