aa89a18f8c5421abaed7549bfc20ca30300c7937
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2025-06-13 16:52:23 +0200

    fix: revert cors change

--- backend/server.ts
+++ backend/server.ts
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5173;
 connectDB();
 
 app.use(bodyParser.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors());
 
 // Register Routes
 app.use("/auth", authRoutes);
